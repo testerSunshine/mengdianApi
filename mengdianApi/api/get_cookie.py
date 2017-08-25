61b056d8af0002ae34531a33cbb6632e3bfbecb3
wenxianping <931128603@qq.com>
2017-08-25 11:14:24 +0800

    first commit

---
+++ mengdianApi/api/get_cookie.py
@@ -0,0 +1,138 @@
+# -*- coding: utf8 -*-
+__author__ = 'MR.wen'
+
+import cookielib
+import sys
+import urllib
+import urllib2
+import requests
+from bs4 import BeautifulSoup
+from requests import RequestException
+reload(sys)
+sys.setdefaultencoding('utf-8')
+
+
+class Get_token():
+    def get_ghs_token(self, account, pwd):
+        data = self._set_ghs_token(account, pwd)
+        return data
+
+    def get_yy_token(self):
+        data = self._set_yy_token()
+        return data
+
+    def get_crm_token(self):
+        data = self._set_crm_token()
+        return data
+
+    def _set_ghs_token(self, account, pwd):
+        """
+        将供货商后台必须的weimob id 赋值给__cookies_ghs
+        """
+        login_info = {'account': account, 'password': pwd}
+        ghsLogin = "http://md.passport.weimobqa.com"  # 供货商和运营的登陆id一样，此处不用配置
+        ghs = "http://md.passport.weimobqa.com/passport/login?callback=http%3A%2F%2Fmd.zs.weimobqa.com%2F&type="
+        return self.get_token(ghsLogin, ghs, login_info)
+
+    def _set_yy_token(self):
+        """
+        获取运营后台必须的weimob id, 赋值给__cookies_yy
+        """
+        YyLogin = "http://md.passport.weimobqa.com"  # 供货商和运营的登陆id一样，此处不用配置
+        yy = "http://md.passport.weimobqa.com/passport/login?callback=http%3A%2F%2Fmd.manager.weimobqa.com%2F&type="
+        login_info = {'account': 'operation', 'password': "111111"}
+        return self.get_token(YyLogin, yy, login_info)
+
+    def _set_crm_token(self):
+        """获取crm登陆token"""
+        url = "http://121.43.101.99:8080/api/login"
+        login_info = {"username": "hbqddls3", "identityCode": "hbqddls3", "password": "hbqddls3", "validateCode": "111"}
+        return self.req_post(url, login_info)
+
+    def up_post(self, url, data=None):
+        """
+        urllib2请求
+        :param url: 请求网址
+        :param data: 请求body
+        :return: header
+        """
+        req = urllib2.Request(url)
+        s_data = urllib.urlencode(data)
+        cookies = cookielib.CookieJar()
+        opener = urllib2.build_opener(urllib2.HTTPCookieProcessor(cookies))
+        result = opener.open(req, s_data)
+        c = dict()
+        for ck in cookies:
+            c[ck.name] = ck.value
+            if c[ck.name] is not None:
+                break
+        return c
+
+    def req_get(self, url, cookies=None):
+        """
+        requests get请求
+        :param url:
+        :param cookies:
+        :return:
+        """
+        if cookies == None:
+            result = requests.get(url=url)
+        else:
+            result = requests.get(url=url, cookies=cookies)
+        try:
+            if result.status_code == 200:
+                return result.content
+            else:
+                return result.status_code
+        except urllib2.HTTPError, e:
+            return e.reason
+
+    def req_post(self, url, data, cookies=None):
+        """
+         requests post请求
+         :param url:
+         :param cookies:
+         :return:
+         """
+        if data is None:
+            raise Exception('date不可缺少...')
+        else:
+            result = requests.post(url=url, cookies=cookies, data=data)
+            if result.status_code == 200:
+                c = dict()
+                for cookie in result.cookies:
+                    c[cookie.name] = cookie.value
+                return c
+            else:
+                return "登陆失败"
+
+    def bs(self, html_data):
+        """
+        解析网页代码
+        :param html_data:
+        :return: soup
+        """
+        soup = BeautifulSoup(html_data, "html.parser")
+        return soup
+
+    def get_token(self, base_url, login_url, dic):
+        """
+        获取后台必须的weimob id
+        :return:  weimob id
+        """
+        try:
+            result = self.req_get(login_url)
+            soup = self.bs(result)
+            action = soup.form['action']  # 加密请求的action文件
+            d = soup.find_all('input')[2]['value']  # 加密请求的d文件
+            playLoad = {'d': d}
+            playLoad.update(dic)
+            cookie = self.up_post(base_url + action, playLoad)
+            return cookie
+        except urllib2.URLError, e:
+            print e.reason
+        except RequestException, e:
+            print e.response
+
+t = Get_token()
+t.get_crm_token()
---
+++ mengdianApi/api/views.py
@@ -0,0 +1,58 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+import json
+from collections import OrderedDict
+
+from django.http import HttpResponse
+from mengdianApi.api.get_cookie import Get_token
+
+
+def index(request):
+    return HttpResponse("Hello Django!")
+
+
+def get_session(request):
+    token = Get_token()
+    if token is not None:
+        body = json.loads(request.body)
+        type = body["type"]
+    else:
+        return HttpResponse(json.dumps({"code": 999999, "message": "无效的格式!"}))
+    if type == "ghs":
+        try:
+            account = body["account"]
+            pwd = body["pwd"]
+            cookie = token.get_ghs_token(account, pwd)
+            r = OrderedDict()
+            data = OrderedDict()
+            data["cookie"] = cookie
+            r["code"] = 0
+            r["msg"] = "success"
+            r["type"] = "ghs"
+            r["data"] = data
+            return HttpResponse(json.dumps(r))
+        except KeyError:
+            return HttpResponse(json.dumps({"code": 999998, "message": "账户名或密码不能为空"}))
+    elif type == "yy":
+        cookie = token.get_yy_token()
+        r = OrderedDict()
+        data = OrderedDict()
+        data["cookie"] = cookie
+        r["code"] = 0
+        r["msg"] = "success"
+        r["type"] = "yy"
+        r["data"] = data
+        return HttpResponse(json.dumps(r))
+    elif type == "crm":
+        cookie = token.get_crm_token()
+        r = OrderedDict()
+        data = OrderedDict()
+        data["cookie"] = cookie
+        r["code"] = 0
+        r["msg"] = "success"
+        r["type"] = "crm"
+        r["data"] = data
+        return HttpResponse(json.dumps(r))
+    else:
+        return HttpResponse(json.dumps({"code": 999999, "message": "type类型不能为空!"}))
+
